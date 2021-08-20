f6218a5634ba15c0477d72c415980db3690e1c5e
Parfait NM <mbamu.developer@gmail.com>
2021-08-20 15:25:17 -0400

    starting the project

---
+++ src/main/java/com/nmparfait/mystore/MystorecumlApplication.java
@@ -0,0 +1,13 @@
+package com.nmparfait.mystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MystorecumlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MystorecumlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nmparfait/mystore/MystorecumlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nmparfait.mystore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MystorecumlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
